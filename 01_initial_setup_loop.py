c8930698e1438cfcbc82d4f21a0473de51f1a17d
Jchench <jessiechen2026@u.northwestern.edu>
2024-07-01 13:55:31 -0400

    ocred 107-056 and 110-343

--- 01_initial_setup_loop.py
+++ 01_initial_setup_loop.py
@@ -21,7 +21,7 @@ def verify_image(image_path):
     return True
 
 # Folder containing the images
-image_folder = "PL111_022"
+image_folder = "PL107_056"
 
 # Get list of image paths from the folder
 image_paths = [os.path.join(image_folder, file) for file in os.listdir(image_folder) if file.lower().split('.')[-1] in ['jpeg', 'png', 'gif', 'webp']]
@@ -76,7 +76,7 @@ for image_path in image_paths:
             content = response_data['choices'][0]['message']['content']
 
             # Define the folder and file path for saving the transcription
-            folder_path = 'clean/PL111_022_results'
+            folder_path = 'clean/PL107_056_results'
             os.makedirs(folder_path, exist_ok=True)
 
             # Use the original image file name for the text file

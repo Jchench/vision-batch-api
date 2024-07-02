ebd736df8b798d26e1c57d2f3a58255c5e9eed2a
Jchench <jessiechen2026@u.northwestern.edu>
2024-07-02 10:19:04 -0400

    compiled sections

--- 01_compile.py
+++ 01_compile.py
@@ -37,9 +37,9 @@ def compile_texts(folder_path, output_file):
         print(f"Error writing to output file: {e}")
 
 # Specify the folder containing the text files
-folder_path = 'clean/PL110_289_results'
+folder_path = 'clean/PL111_203_results'
 
 # Specify the name of the output file
-output_file = 'clean/PL110_289_results/PL110_289.txt'
+output_file = 'clean/PL111_203_results/PL111_203.txt'
 
 compile_texts(folder_path, output_file)

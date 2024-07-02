5549ef626a393f7941b970d6922f9ea6a4cf5c60
Jchench <jessiechen2026@u.northwestern.edu>
2024-07-02 10:45:15 -0400

    104-208 set up

--- 00_split_pdf.py
+++ 00_split_pdf.py
@@ -1,26 +1,19 @@
 from pdf2image import convert_from_path
 import os
 
-# Path to the folder containing PDF files
-input_folder = 'pdf'
+# Path to the PDF file
+pdf_path = 'pdf/PL104_208.pdf'
 
-# List all files in the input folder
-pdf_files = [f for f in os.listdir(input_folder) if f.endswith('.pdf')]
+# Path to the directory where images will be saved
+output_folder = pdf_path[:-4]  # Remove the .pdf extension for folder name
 
-for pdf_file in pdf_files:
-    # Path to the PDF file
-    pdf_path = os.path.join(input_folder, pdf_file)
+# Create the directory if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
     
-    # Path to the directory where images will be saved
-    output_folder = os.path.join(input_folder, pdf_file[:-4])  # Remove the .pdf extension for folder name
+# Convert PDF to images
+images = convert_from_path(pdf_path)
 
-    # Create the directory if it doesn't exist
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    
-    # Convert PDF to images
-    images = convert_from_path(pdf_path)
-    
-    # Save each image as JPEG in the output folder
-    for i, image in enumerate(images):
-        image.save(os.path.join(output_folder, f'page_{i+1}.jpeg'), 'JPEG')
+# Save each image as JPEG in the output folder
+for i, image in enumerate(images):
+    image.save(os.path.join(output_folder, f'page_{i+1}.jpeg'), 'JPEG')

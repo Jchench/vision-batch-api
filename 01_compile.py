70be51c08c7d1d5f053a216e8275cf1d87869488
Jchench <jessiechen2026@u.northwestern.edu>
2024-07-16 16:36:34 -0400

    compiled last file

--- 01_compile.py
+++ 01_compile.py
@@ -37,9 +37,9 @@ def compile_texts(folder_path, output_file):
         print(f"Error writing to output file: {e}")
 
 # Specify the folder containing the text files
-folder_path = 'clean/PL102_550_4044_results'
+folder_path = 'clean/PL115_174_results'
 
 # Specify the name of the output file
-output_file = 'clean/results/PL102_550_4044.txt'
+output_file = 'clean/results/PL115_174.txt'
 
 compile_texts(folder_path, output_file)
